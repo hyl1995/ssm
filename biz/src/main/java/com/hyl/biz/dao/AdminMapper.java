725a32157a9b45bc49063d4d88b6dc7bcd68babf
Yuly Huang <592443616@qq.com>
2019-05-31 21:58:44 +0800

    update mapper

--- biz/src/main/java/com/hyl/biz/dao/AdminMapper.java
+++ biz/src/main/java/com/hyl/biz/dao/AdminMapper.java
@@ -8,18 +8,18 @@ import java.util.List;
 @Mapper
 public interface AdminMapper {
 
-    @Insert(" insert into Admin_ ( name ) values (#{name}) ")
+    @Insert(" insert into admin ( id,nick_name,pass,name,phone ) values (#{id},#{nickName},#{pass},#{name},#{phone}) ")
     public int add(Admin admin);
 
-    @Delete(" delete from Admin_ where id= #{id} ")
-    public void delete(int id);
-
-    @Select("select * from Admin_ where id= #{id} ")
-    public Admin get(int id);
-
-    @Update("update Admin_ set name=#{name} where id=#{id} ")
+    @Update("update admin set nick_name=#{nickName},pass=#{pass},name=#{name},phone=#{phone} where id=#{id} ")
     public int update(Admin admin);
 
-    @Select(" select * from Admin_ ")
+    @Select("select * from admin where id= #{id} ")
+    public Admin get(int id);
+
+    @Delete(" delete from admin where id= #{id} ")
+    public void delete(int id);
+
+    @Select(" select * from admin ")
     public List<Admin> list();
 }
