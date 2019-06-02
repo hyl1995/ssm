ba6e4cbeba55bdb110104566523fe12a3b5f11da
Yuly Huang <592443616@qq.com>
2019-06-02 21:20:35 +0800

    pagehelper

--- biz/src/main/java/com/hyl/biz/dao/AdminMapper.java
+++ biz/src/main/java/com/hyl/biz/dao/AdminMapper.java
@@ -9,17 +9,24 @@ import java.util.List;
 public interface AdminMapper {
 
     @Insert(" insert into admin ( id,nick_name,pass,name,phone ) values (#{id},#{nickName},#{pass},#{name},#{phone}) ")
-    public int add(Admin admin);
+    public int add(@Param("Admin")Admin admin);
 
     @Update("update admin set nick_name=#{nickName},pass=#{pass},name=#{name},phone=#{phone} where id=#{id} ")
-    public int update(Admin admin);
+    public int update(@Param("Admin")Admin admin);
 
     @Select("select * from admin where id= #{id} ")
-    public Admin get(int id);
+    public Admin get(@Param("id")int id);
 
     @Delete(" delete from admin where id= #{id} ")
-    public void delete(int id);
+    public void delete(@Param("id")int id);
 
-    @Select(" select * from admin ")
-    public List<Admin> list();
+    @Select(" select * from admin <where><if test=\"filter!=null && filter!=''\">${filter}</if></where> ")
+    public Page<Admin> selectByPage(@Param("filter")String filter);
+
+    @Select(" select * from admin <where><if test=\"filter!=null && filter!=''\">${filter}</if></where> ")
+    public List<Admin> selectByList(@Param("filter")String filter);
+
+    @Select(" select * from admin <where><if test=\"filter!=null && filter!=''\">${filter}</if></where> ")
+    public Admin selectBySingle(@Param("filter")String filter);
 }
+
