96d19400162dc778c341cc80ea9c94873409c163
Yuly Huang <592443616@qq.com>
2019-06-30 13:28:14 +0800

    FileRead

--- core/src/main/java/com/hyl/core/util/FileRead.java
+++ core/src/main/java/com/hyl/core/util/FileRead.java
@@ -2,15 +2,57 @@ package com.hyl.core.util;
 
 import cn.hutool.core.io.FileUtil;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FileRead {
+    static Set<String> packgeSet = new HashSet<>();
+
     public static void main(String[] args) {
-        File[] ls = FileUtil.ls("E:\\Git\\ssm");
-        for (File l : ls) {
-            System.out.println(l.getName()+ ": "+ l.getAbsolutePath());
+        File[] ls = getFiles("E:\\Git\\ssm");
+//        for (File l : ls) {
+//            System.out.println(l.getName()+ ": "+ l.getAbsolutePath());
+//        }
+
+        loopFiles(ls);
+
+        for (String s : packgeSet) {
+            System.out.println(s);
         }
     }
 
+    private static void loopFiles(File[] ls) {
+        for (File l : ls) {
+            if (l.isFile()) {
+                readJava(l);
+            }
+            if (l.isDirectory()) {//目录
+                File[] files = getFiles(l.getPath());
+                loopFiles(files);
+            }
+        }
+    }
 
+    private static File[] getFiles(String path) {
+        File[] ls = FileUtil.ls(path);
+        return ls;
+    }
+
+    private static String readJava(File file) {
+        boolean isJava = file.getName().endsWith(".java");
+        if (isJava) {//java文件
+            BufferedReader reader = FileUtil.getReader(file, "utf-8");
+            try {
+                String packgeName = reader.readLine();
+                packgeSet.add(packgeName);
+                return packgeName;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return "";
+    }
 }
