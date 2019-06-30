a12537610413f16c1e5c2109c407680dbfe09740
Yuly Huang <592443616@qq.com>
2019-06-30 11:58:45 +0800

    FileRead

--- core/src/main/java/com/hyl/core/util/FileRead.java
+++ core/src/main/java/com/hyl/core/util/FileRead.java
@@ -1,5 +1,16 @@
 package com.hyl.core.util;
 
+import cn.hutool.core.io.FileUtil;
+
+import java.io.File;
+
 public class FileRead {
+    public static void main(String[] args) {
+        File[] ls = FileUtil.ls("E:\\Git\\ssm");
+        for (File l : ls) {
+            System.out.println(l.getName()+ ": "+ l.getAbsolutePath());
+        }
+    }
+
 
 }
