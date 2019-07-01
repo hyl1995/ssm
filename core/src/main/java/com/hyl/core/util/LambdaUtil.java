45e594c7e8158ddcff8b9d1a2e0fd589678ab4dd
hyl1995 <592443616@qq.com>
2019-07-01 09:51:41 +0800

    LambdaUtil

--- core/src/main/java/com/hyl/core/util/LambdaUtil.java
+++ core/src/main/java/com/hyl/core/util/LambdaUtil.java
@@ -1,8 +1,11 @@
 package com.hyl.core.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class LambdaUtil {
@@ -14,8 +17,16 @@ public class LambdaUtil {
         return joinByDelimiter(list, key, ",");
     }
 
+    public static <T> String joinByCommaWithNotBlank(List<T>list, Function key) {
+        return joinByDelimiterWithNotBlank(list, key, ",");
+    }
+
     public static <T> String joinByDelimiter(List<T>list, Function key, String delimiter) {
         return list.stream().map(key).collect(Collectors.joining(delimiter)).toString();
     }
 
+    public static <T> String joinByDelimiterWithNotBlank(List<T>list, Function key, String delimiter) {
+        Predicate<String> predicate = s -> StringUtils.isNotBlank(s);
+        return list.stream().map(key).filter(predicate).collect(Collectors.joining(delimiter)).toString();
+    }
 }
