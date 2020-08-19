fb5417eb6f002f7364cc93421f5c78fa4ffd91ba
hyl <592443616@qq.com>
2020-08-19 11:22:47 +0800

    Swagger2PostmanUtil类更新

--- core/src/main/java/com/hyl/core/util/Swagger2PostmanUtil.java
+++ core/src/main/java/com/hyl/core/util/Swagger2PostmanUtil.java
@@ -4,6 +4,7 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.ClassUtil;
 import cn.hutool.core.util.ReflectUtil;
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.hyl.core.annotation.ApiVersion;
@@ -12,6 +13,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.File;
 import java.lang.annotation.Annotation;
@@ -24,10 +26,12 @@ import java.util.Set;
  * swagger转postman json文件
  */
 public class Swagger2PostmanUtil {
+    public static final String name = "课程购买";
+    public static final String version = "API_3.2.0";
+    public static final String token = "";
+
     public static void main(String[] args) {
-        String name = "话题";
-        String version = "API_3.2.0";
-        String token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjVCQzcxQzkwQTUwOTVCNjA4OEU4MDQ1QTk3RDE1REIyQUJCOEQ4QzUiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJXOGNja0tVSlcyQ0k2QVJhbDlGZHNxdTQyTVUifQ.eyJuYmYiOjE1OTcxMDkzNzQsImV4cCI6MTU5NzE2Njk3NCwiaXNzIjoiaHR0cHM6Ly90c2FkbWluLnF1YW56aGFvLmNvIiwiYXVkIjpbImh0dHBzOi8vdHNhZG1pbi5xdWFuemhhby5jby9yZXNvdXJjZXMiLCJBY2NvdW50QXBpIiwiTWVtYmVyQXBpIiwiTVlXQXBpIiwiUGF5QXBpIiwic21zYXBpIl0sImNsaWVudF9pZCI6Ik1lbWJlckZyb250IiwiUHJvamVjdEtleSI6IjI5YTAzZGEzMmY0ODQ0YTliYTBlNjdhNGUwMDg2MmY2IiwiVXNlclNpZ25JZCI6IjEzIiwic3ViIjoiOTUwIiwiYXV0aF90aW1lIjoxNTk3MTA5Mzc0LCJpZHAiOiJsb2NhbCIsInNjb3BlIjpbImN1c3RvbS5wcm9maWxlIiwiZW1haWwiLCJvcGVuaWQiLCJwcm9maWxlIiwicm9sZXMiLCJBY2NvdW50QXBpIiwiTWVtYmVyQXBpIiwiTVlXQXBpIiwiUGF5QXBpIiwic21zYXBpLnNlbmQiLCJvZmZsaW5lX2FjY2VzcyJdLCJhbXIiOlsicHdkIl19.ICdWlbGTYvnYKD8YiTHhaLjAl8z_IdaAw5YDvcrby2wY8UCd1wy4vv6n_mRhoQIvZI14kcjmPRYjXT6e1IZkfjqZYrmWSmp55kyH_iFhmF8MR9117trl50QR5vpMzoJlMh5gamPFSLZaNlW2Q-LO7Wf7HrmX7Tyla2jLh1kMU9pJFDvuLZdSR-f6llZbQdA-Ra2qy-PR738-eSeA1jzKQmGCmpMVRfYirsxffu2cI5U1Q4kHz9Sp8vNJflKtmYMlBI9lo9D5TdIfAFP1TGuCPggT_KZ_KuzDajuXGh3F8xgX50JccvCUWqaS3BOvucL37QtxS8Avr9Zr9YF3ebxteA";
+
         JSONObject jsonObject = new JSONObject();
 
         JSONObject info = new JSONObject();
@@ -39,9 +43,9 @@ public class Swagger2PostmanUtil {
         JSONObject protocolProfileBehavior = new JSONObject();
         jsonObject.put("protocolProfileBehavior", protocolProfileBehavior);
 
-        jsonObject.put("item", scanPackge(version, token));
+        jsonObject.put("item", scanPackge());
 
-        jsonObject.put("auth", authFromParent(token));
+        jsonObject.put("auth", authFromParent());
 
         File file = FileUtil.newFile("C:\\postmanJson\\" + name + ".postman_collection.json");
         FileUtil.writeUtf8String(jsonObject.toJSONString(), file);
@@ -51,28 +55,40 @@ public class Swagger2PostmanUtil {
     /**
      * 扫包 获取接口
      *
-     * @param token
-     * @param version
      * @return
      */
-    private static JSONArray scanPackge(String version, String token) {
+    private static JSONArray scanPackge() {
         JSONArray item = new JSONArray();
-        Set<Class<?>> set = ClassUtil.scanPackage("com.alib.controller");
+        Set<Class<?>> set = ClassUtil.scanPackage("com.startom.web.controller.OrderController");
         set.forEach(clazz -> {
             Method[] methods = clazz.getMethods();
             for (Method method : methods) {
-
                 ApiVersion at = method.getAnnotation(ApiVersion.class);
                 if (at != null) {
-                    if (checkVersion(at, version)) {
-                        System.out.println(clazz.getName() + " " + method.getName());
+                    if (checkVersion(at)) {
+                        System.out.println(clazz.getName());
+                        System.out.println(method.getName());
                         String uriPath = uriPathOfClass(clazz);
-                        item.add(item(method, uriPath, token));
+                        item.add(item(method, uriPath));
                     }
                 }
             }
         });
         return item;
+    }
+
+    /**
+     * 检查版本
+     * @param at
+     * @return
+     */
+    private static boolean checkVersion(ApiVersion at) {
+        for (String s : at.group()) {
+            if (version.equals(s)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
@@ -88,22 +104,7 @@ public class Swagger2PostmanUtil {
         return "";
     }
 
-    /**
-     * 检查版本
-     * @param at
-     * @param version
-     * @return
-     */
-    private static boolean checkVersion(ApiVersion at, String version) {
-        for (String s : at.group()) {
-            if (version.equals(s)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static JSONObject item(Method method, String uriPath, String token) {
+    private static JSONObject item(Method method, String uriPath) {
         JSONObject object = new JSONObject();
         JSONArray response = new JSONArray();
         object.put("response", response);
@@ -118,31 +119,32 @@ public class Swagger2PostmanUtil {
             if (annotation.annotationType().equals(PostMapping.class)) {
                 request.put("method", "POST");
                 PostMapping mapping = (PostMapping) annotation;
-                request.put("url", url(uriPath, mapping.value()[0]));
-                request.put("body", body(method));
+                request.put("url", postUrl(uriPath, mapping.value()[0]));
+                request.put("body", postBody(method));
             } else if (annotation.annotationType().equals(GetMapping.class)) {
                 request.put("method", "GET");
-                PostMapping mapping = (PostMapping) annotation;
-                request.put("url", url(uriPath, mapping.value()[0]));
+                GetMapping mapping = (GetMapping) annotation;
+                request.put("url", getUrl(uriPath, mapping.value()[0], method));
+                request.put("body", getBody());
             }
 
         }
         //传递token用
-//        request.put("header", header(token));
-//        request.put("auth", auth(token));
+//        request.put("header", header());
+//        request.put("auth", auth());
         object.put("request", request);
         return object;
     }
 
-    private static JSONObject authFromParent(String token) {
-        return auth(token);
+    private static JSONObject authFromParent() {
+        return auth();
     }
 
     /**
      * header参数
      * @return
      */
-    private static JSONArray header(String token) {
+    private static JSONArray header() {
         JSONArray header = new JSONArray();
         JSONObject auth = new JSONObject();
         auth.put("key", "Authorization");
@@ -152,7 +154,7 @@ public class Swagger2PostmanUtil {
         return header;
     }
 
-    private static JSONObject auth(String token) {
+    private static JSONObject auth() {
         JSONObject auth = new JSONObject();
         auth.put("type", "bearer");
         JSONArray bearer = new JSONArray();
@@ -164,20 +166,6 @@ public class Swagger2PostmanUtil {
         bearer.add(bearerToken);
         auth.put("bearer", bearer);
         return auth;
-    }
-
-    private static JSONObject body(Method method) {
-        JSONObject body = new JSONObject();
-        body.put("mode", "raw");
-        body.put("raw", raw(method.getParameters()));
-
-        JSONObject options = new JSONObject();
-        JSONObject raw = new JSONObject();
-        raw.put("language", "json");
-        options.put("raw", raw);
-        body.put("options", options);
-
-        return body;
     }
 
     /**
@@ -239,7 +227,7 @@ public class Swagger2PostmanUtil {
         return params.toJSONString();
     }
 
-    private static JSONObject url(String uriPath, String suffix) {
+    private static JSONObject postUrl(String uriPath, String suffix) {
         String path = uriPath + suffix;
         JSONObject url = new JSONObject();
         url.put("raw", "http://{{host}}/" + path);
@@ -247,6 +235,64 @@ public class Swagger2PostmanUtil {
         url.put("host", "{{host}}");
         url.put("path", pathArray(path));
         return url;
+    }
+
+    private static JSONObject postBody(Method method) {
+        JSONObject body = new JSONObject();
+        body.put("mode", "raw");
+        body.put("raw", raw(method.getParameters()));
+
+        JSONObject options = new JSONObject();
+        JSONObject raw = new JSONObject();
+        raw.put("language", "json");
+        options.put("raw", raw);
+        body.put("options", options);
+
+        return body;
+    }
+
+    private static JSONObject getUrl(String uriPath, String suffix, Method method) {
+        String path = uriPath + suffix;
+        JSONObject url = new JSONObject();
+        url.put("raw", "http://{{host}}/" + path);
+        url.put("protocol", "http");
+        url.put("host", "{{host}}");
+        url.put("path", pathArray(path));
+        url.put("query", query(method.getParameters()));
+        return url;
+    }
+
+    private static JSONObject getBody() {
+        JSONObject body = new JSONObject();
+        body.put("mode", "urlencoded");
+        body.put("urlencoded", new JSONArray());
+
+        JSONObject options = new JSONObject();
+        JSONObject raw = new JSONObject();
+        raw.put("language", "json");
+        options.put("raw", raw);
+        body.put("options", options);
+
+        return body;
+    }
+
+    /**
+     * 获取参数
+     * @param parameters
+     * @return
+     */
+    private static JSONArray query(Parameter[] parameters) {
+        JSONArray query = new JSONArray();
+        for (Parameter parameter : parameters) {
+            RequestParam at = parameter.getAnnotation(RequestParam.class);
+            if (at != null) {
+                JSONObject params = new JSONObject();
+                params.put("key", StrUtil.isNotBlank(at.name()) ? at.name() : at.value());
+                params.put("value", "");
+                query.add(params);
+            }
+        }
+        return query;
     }
 
     private static JSONArray pathArray(String path) {
